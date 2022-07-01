4fc4f59a4a7e0b0a7fda7132e521dbb50cd1aa9c
isachomonik <dev.isachomonik@gmail.com>
2022-07-01 17:43:58 -0300

    Função que busca e retorna o cachorro baseado no id

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,40 @@
-const cachorros = require('./database/cachorros.json');
+const cachorros = require("./database/cachorros.json");
+const fs = require("fs");
+const path = require("path");
 
-function salvar(){}
+// const newCachorros = JSON.parse(cachorros);
 
-function buscar(id){}
+function salvar() {
+  let arquivo = path.resolve("./database/cachorros.json");
+  let json = JSON.stringify(cachorros, null, 4);
 
-module.exports = {
-    
+  fs.writeFileSync(arquivo, json);
+
+  // console.log('arquivo: ' + arquivo + ' json: ' + json);
 }
+
+// salvar();
+
+function buscar(idBuscado) {
+
+  // idBuscado = 2;
+  function mesmoId(cachorro){
+    if(cachorro.id == idBuscado){
+      return true;
+
+    } else {
+      return false;
+    }
+  }    
+
+  return cachorros.find(mesmoId);
+
+  
+  
+}
+
+// buscar(3);
+
+// console.table(resultado);
+
+module.exports = {};
