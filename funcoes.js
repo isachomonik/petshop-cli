81aaac3fa083a1c8da11133dfaa20d9389e27d0f
isachomonik <dev.isachomonik@gmail.com>
2022-07-07 09:56:04 -0300

    atualização da função adicionar

--- funcoes.js
+++ funcoes.js
@@ -7,18 +7,13 @@ const { map } = require("./settings/servicos");
 const { captureRejections } = require("events");
 const { get } = require("http");
 
-
-
 // função que salva o arquivo em JSON
 function salvar() {
   let arquivo = path.resolve("./database/cachorros.json");
   let json = JSON.stringify(cachorros, null, 4);
 
   fs.writeFileSync(arquivo, json);
-
-
 }
-
 
 // função que busca e retorna um cachorro pelo ID
 function buscar(idBuscado) {
@@ -47,8 +42,10 @@ function descrever(idBuscado) {
 
 //função que adiciona um novo cachorro ao fim da lista
 function adicionar(cachorroNovo) {
+  
+
   let novoCachorro = {
-    id: cachorros.length + 1,
+    id: cachorros[cachorros.length - 1].id + 1,
     nome: cachorroNovo.nome,
     sexo: cachorroNovo.sexo,
     castrado: cachorroNovo.castrado,
@@ -64,60 +61,63 @@ function adicionar(cachorroNovo) {
 }
 
 //função que retorna a data atual
-function getDate(){
+function getDate() {
   var today = new Date();
- var data = today.toISOString().substring(0, 10);
+  var data = today.toISOString().substring(0, 10);
 
-  return data.slice(0,10)
+  return data.slice(0, 10);
 }
 
 //função que adiciona vacina ao cachorro selecionado
 function vacinar(id, vacina, dataVacina) {
-
   let cachorro = buscar(id);
   dataVacina = getDate();
   let dadosVacina = {
     nome: vacina,
-    data: dataVacina
+    data: dataVacina,
   };
 
-  if(cachorro.id){
+  if (cachorro.id) {
     cachorro.vacinas.push(dadosVacina);
   } else {
-   console.log("Cachorro inexistente!")
-  };
+    console.log("Cachorro inexistente!");
+  }
 
   salvar();
-};
+}
 
 //função que adiciona serviço ao cachorro selecionado
 function atribuirServico(id, servico, dataServico) {
-
   let cachorro = buscar(id);
   dataServico = getDate();
 
   let dadosServico = {
     nome: servico,
-    data: dataServico
+    data: dataServico,
   };
 
-  if(cachorro.id){
+  if (cachorro.id) {
     cachorro.servicos.push(dadosServico);
   } else {
-   console.log("Cachorro inexistente!")
-  };
+    console.log("Cachorro inexistente!");
+  }
 
   salvar();
-};
-
-//função que remove um cachorro da lista
-function remover(id){
-  let cachorro = buscar(id);
-  cachorros.splice(cachorros.indexOf(cachorro), 1)
-
-  salvar()
 }
 
+//função que remove um cachorro da lista
+function remover(id) {
+  let cachorro = buscar(id);
+  cachorros.splice(cachorros.indexOf(cachorro), 1);
 
+  salvar();
+}
 
-module.exports = { remover, adicionar, atribuirServico, vacinar, listar, descrever};
+module.exports = {
+  remover,
+  adicionar,
+  atribuirServico,
+  vacinar,
+  listar,
+  descrever,
+};
